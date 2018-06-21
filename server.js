44e1467b3855d64017a70e44cec5b7e276a41f06
Adama Diallo <adamadial58@me.com>
2018-06-21 14:18:13 +0400

    creation connexion auà la bdd et update fichier README

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // initialisation du server
 const express = require('express');
 const app = express();
-var port = 3013;
+var port = 4013;
 
 
 app.use(express.static('public'));
@@ -12,7 +12,21 @@ app.get('/', function(req,res){
     res.sendFile(__dirname+'/index.html')
 });
 
+// connexion a bdd
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
 
+
+// Connection URL
+var url = 'mongodb://localhost:27017/reservation';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    db.close();
+});
 
 // port d'écoute du serveur
 
