405432595c1ef69cc48c9f81207ce91aed5ca1dc
anna974 <annapiaraly@gmail.com>
2018-06-21 16:35:46 +0400

    ajout d'une fonction mise à jour hotels

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // initialisation du server
 const express = require('express');
 const app = express();
-var port = 3013;
+const bodyParser = require('body-parser');
+var port = 4013;
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static('static'));
 
 // afficher l'index
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname+'/index.html')
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
 });
 
 
@@ -17,88 +19,24 @@ app.get('/', function(req,res){
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
-app.get('/get_clients', function(req,res){
+app.get('/get_clients', function (req, res) {
 
-    //
-    get_clients(function(clients){
-        console.log(clients);
-        res.send(clients);
-    });
+    // mongodb vers clients
 
-    //
 
 });
 
+app.get('/get_hotels', function (req, res) {
 
-function get_clients(cb){
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("reservation");
-        dbo.collection("clients").find({}).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            //res.send(result);
-            cb(result);
-            db.close();
-        });
-    });
-
-}
-
-
-app.get('/get_hotels', function(req,res){
-   
     // mongodb vers hotels
-    //
-    get_hotels(function(hotels){
-        console.log(hotels);
-        res.send(hotels);
-    });
 
-    //
 });
 
-function get_hotels(cb){
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("reservation");
-        dbo.collection("hotels").find({}).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            //res.send(result);
-            cb(result);
-            db.close();
-        });
-    });
+app.get('/get_secteurs', function (req, res) {
 
-}
-
-app.get('/get_secteurs', function(req,res){
-   
     // mongodb vers secteurs
-    //
-    get_secteurs(function(secteurs){
-        console.log(secteurs);
-        res.send(secteurs);
-    });
-    //
+
 });
-
-
-function get_secteurs(cb){
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("reservation");
-        dbo.collection("secteurs").find({}).toArray(function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            //res.send(result);
-            cb(result);
-            db.close();
-        });
-    });
-
-}
 
 // Connection URL
 var url = 'mongodb://localhost:27017/reservation';
@@ -111,6 +49,36 @@ MongoClient.connect(url, function (err, db) {
     db.close();
 });
 
-app.listen(port, function(){
-    console.log('on')
-})
+app.get('/admin/ajout-hotel', function (req, res) {
+    res.sendFile(__dirname + '/ajout-hotel.html')
+});
+
+
+// A REVOIR - ne récupère pas les données dans newvalues
+
+app.put('/update', function (req, res) {
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("reservation");
+
+        var monid = req.body.donnee1;
+        var name = req.body.donnee2;
+        var image = req.body.donnee3;
+        var mark = req.body.donnee4;
+        var secteur = req.body.donnee5;
+
+
+        var myquery = { id: monid };
+        var newvalues = { $set: { Nom: name, img: image, id_secteur: secteur, Nb_etoiles: mark } };
+        console.log(newvalues);
+        dbo.collection("hotels").updateOne(myquery, newvalues, function (err, result) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close()
+
+        });
+    });
+});
+
+app.listen(4013);
