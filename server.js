1c29524636ceaf886dcefde903fb174906c85245
kafsalon <jfglamport@gmail.com>
2018-06-22 14:27:19 +0400

    ajout update et suppression hotels via admin

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
-var port = 4013;
+var port = 3009;
 // connexion a bdd
+const mongodb = require('mongodb');
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
@@ -177,13 +178,44 @@ app.put('/update', function (req, res) {
 app.get('/admin/hotels', function (req, res) {
     get_hotels(function(hotels){
         //console.log(hotels);
-        res.render('admin/index', {
+        res.render('admin/hotels', {
             hotels: hotels
         });
         // res.send(hotels);
     });
 });
 
+app.get('/admin/', function (req, res) {
+    get_hotels(function(hotels){
+        //console.log(hotels);
+        res.render('admin/index', {
+            hotels: hotels
+        });
+        // res.send(hotels);
+    });
+});
+/**
+ * Route del d'un doc via le formulaire 
+ */
+app.delete('/hotels/:_id', function (req, res) {
+    const _id = req.params._id;
+  
+    MongoClient.connect(url, function (err, database) {
+      if (err) throw err;
+      var myquery = {
+        _id: mongodb.ObjectId(_id)
+      };
+      var dbo = database.db("reservation");
+      //console.log(newvalues);
+       dbo.collection("hotels").deleteOne(myquery, function (err, obj) {
+        if (err) throw err;
+       // console.log(obj);
+        database.close();
+      });
+    });
+      
+    res.end("id= " + _id + " supprimé");
+  });
 
 app.listen(port, function(){
     console.log('the port is on');
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,16 @@
 $('document').ready(function () {
     // appel de la fonction pour ajout des etoiles automatiquement pour les hotels
     stars();
-
-    var val1 = $('#id-h').val();
+    var port = 3009;
+    //var val1 = $('#id-h').val();
     // var val2 = $('#name-hotel').val();
     // var val3 = $('#img-hotel').val();
     // var val4 = $('#mark-hotel').val();
     // var val5 = $('#secteur-hotel').val();
 
-    
-    $('#add').click(function () {
+   // console.log("eee");
+    $('#update').click(function () {
+        console.log("eee");
         var val1 = $('#id-h').val();
         var val2 = $('#name-hotel').val();
         var val3 = $('#img-hotel').val();
@@ -24,13 +25,34 @@ $('document').ready(function () {
         
     
         $.ajax({
-            url: 'http://localhost:4013/update',
+            url: 'http://localhost:' + port + '/update',
             method: "PUT",
             data: { donnee1: val1, donnee2: val2, donnee3: val3, donnee4: val4, donnee5: val5 },
             success: function (data) {
+                
+                $('#update_message').html('<div class="alert alert-success" role="alert">Données enregistrées !</div>');
 
-                $('#message').html('<p>Données enregistrées ! </p>');
+            }
+        });
+    });
 
+    /* */
+    $('a.delete').click(function (event) {   
+        // e.preventDefault;  
+        // recuperation de l'_ID via l'attribut data-objid        
+        var _id = $(this).data('objid');    
+       console.log("id",_id);
+        $.ajax({
+            url: 'http://localhost:' + port + '/hotels/' + _id,
+            type: 'DELETE',
+            data: {
+                _id: _id
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (e) {
+                console.error("erreur :", e);
             }
         });
     });
